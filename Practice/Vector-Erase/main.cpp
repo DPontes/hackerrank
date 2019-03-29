033416014ee2edde3a7412501548e8bbd270cc2a
Diogo Pontes <diogo.pontes@zenuity.com>
2019-03-29 15:58:25 +0100

    Terrible hack, but it works and passes all TCs

--- Practice/Vector-Erase/main.cpp
+++ Practice/Vector-Erase/main.cpp
@@ -7,6 +7,33 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int v_length;
+    vector<int> v{};
+
+    cin >> v_length;
+
+    for(int i = 0; i < v_length; i++) {
+        int value;
+        cin >> value;
+
+        v.push_back(value);
+    }
+
+    int remove_this;
+    int remove_that;
+
+    cin >> remove_this;
+
+    v.erase(v.begin()+remove_this-1);
+
+    cin >> remove_this >> remove_that;
+
+    v.erase(v.begin()+remove_this-1, v.begin()+remove_that-1);
+
+    cout << v.size() << endl;
+    for(int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+
     return 0;
 }
