987d82707c16e8d848774c31111a4d6870d7dee2
Diogo Pontes <diogo.pontes@zenuity.com>
2019-03-29 13:53:51 +0100

    fills a dynamic vector with strings from input

--- Practice/AttributeParser/main.cpp
+++ Practice/AttributeParser/main.cpp
@@ -6,9 +6,16 @@
 using namespace std;
 
 int main() {
-    int value;
-    cin >> value;
-    cout << value;
+    int n_lines, n_queries;
+    vector<string> v;
+    string line;
+    cin >> n_lines;
+    cin >> n_queries;
+
+    while(getline(cin, line)) {
+        v.push_back(line);
+    }
+
     return 0;
 }
 
