1c4ec14fde24509bf33f682302af596490b61125
DPontes <dpontes@protonmail.com>
2019-03-30 22:49:28 +0100

    Works on most TCs, fails on some due to timeout

--- Practice/Lower-Bound-STL/main.cpp
+++ Practice/Lower-Bound-STL/main.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <string>
 #include <cstdio>
 #include <vector>
 #include <iostream>
@@ -7,6 +8,29 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n_integers, n_queries, query;
+    vector<int> v;
+
+    cin >> n_integers;
+
+    for(int i = 0; i<n_integers;i++) {
+        int value;
+        cin >> value;
+        v.push_back(value);
+    }
+
+    cin >> n_queries;
+    vector<int>::iterator low;
+   
+    for (int i = 0; i < n_queries; i++) {
+        cin >> query;
+        low = lower_bound(v.begin(), v.end(), query);
+        if(find(v.begin(), v.end(), query) != v.end()) {
+            cout << "Yes ";
+        } else {
+            cout << "No ";
+        }
+        cout << (low - v.begin() + 1) << endl;
+    }
     return 0;
 }
