622c0de20a817e50c7e3dbc238b82e251870505e
DPontes <dpontes@protonmail.com>
2019-04-01 20:51:14 +0200

    Works and passes all TCs

--- Practice/Sets-STL/main.cpp
+++ Practice/Sets-STL/main.cpp
@@ -8,6 +8,29 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n_queries, action, value;
+    set<int> values;
+    set<int>::iterator iter;
+
+    cin >> n_queries;
+
+    for(int i = 0; i < n_queries; i++) {
+        cin >> action >> value;
+        switch(action) {
+            case 1:
+                values.insert(value);
+                break;
+            case 2:
+                values.erase(value);
+                break;
+            case 3:
+                values.find(value) != values.end() ? cout << "Yes" : cout << "No";
+                cout << endl;
+                break;
+            default:
+                break;
+        }
+    }
+
     return 0;
 }
