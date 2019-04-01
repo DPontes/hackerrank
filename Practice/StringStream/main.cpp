4f78476334762fc5d7c4644f72f09aaedb2cdc2b
DPontes <dpontes@protonmail.com>
2019-04-01 21:13:16 +0200

    Works and passes all TCs

--- Practice/StringStream/main.cpp
+++ Practice/StringStream/main.cpp
@@ -1,10 +1,17 @@
-include <sstream>
+#include <sstream>
 #include <vector>
+#include <string>
 #include <iostream>
 using namespace std;
 
 vector<int> parseInts(string str) {
-        // Complete this function
+    vector<int> values;
+    string value;
+    stringstream ss(str);
+    while(getline(ss, value, ',')) {
+        values.push_back(stoi(value));
+    }
+    return values;    
 }
 
 int main() {
