f7f7361e52a6cfd2dee268cfab679495cb898b71
DPontes <dpontes@protonmail.com>
2019-04-03 22:01:25 +0200

    Works and passes all TCs

--- Practice/Vector-Sort/main.cpp
+++ Practice/Vector-Sort/main.cpp
@@ -6,5 +6,16 @@
 using namespace std;
 
 int main() {
+    vector<int> values;
+    int value, n_integers;
+
+    cin >> n_integers;
+    for(int i=0; i < n_integers;i++) {
+        cin >> value;
+        values.push_back(value);
+    }
+
+    sort(values.begin(), values.end());
+    for(auto a : values) { cout << a << " "; }
     return 0;
 }
