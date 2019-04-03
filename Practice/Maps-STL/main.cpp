15b4c67f3e080a3c4a84dea2f9982b4cfcdf4f6a
DPontes <dpontes@protonmail.com>
2019-04-03 23:00:41 +0200

    Works and passes all TCs

--- Practice/Maps-STL/main.cpp
+++ Practice/Maps-STL/main.cpp
@@ -2,12 +2,35 @@
 #include <cstdio>
 #include <vector>
 #include <iostream>
+#include <sstream>
 #include <set>
 #include <map>
+#include <utility>
 #include <algorithm>
 using namespace std;
 
 int main() {
-    
+    map<string, int>marks;
+    int n_queries, grade, action;
+    string name;
+
+    cin >> n_queries;
+
+    for(int i=0; i<n_queries; i++) {
+        cin >> action >> name;
+        switch(action) {
+            case 1:
+                cin >> grade;
+                marks[name] += grade;
+                break;
+            case 2:
+                marks.erase(name);
+                break;
+            case 3:
+                cout << marks[name] << endl;
+                break;
+        }
+    }
+
     return 0;
 }
