e3b02ecf04948333d5889720ee0077a04609eb2e
DPontes <dpontes@protonmail.com>
2019-04-14 14:10:52 +0200

    Works but doesn't pass with input3.txt

--- InterviewPrep/JumpingClouds/main.cpp
+++ InterviewPrep/JumpingClouds/main.cpp
@@ -1,11 +1,18 @@
-#include <bit/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 vector<string> split_string(string);
 
 int jumpingOnClouds(vector<int> c) {
-
+    int jumps = 0;
+    int it = 0;
+    while(it != (c.size()-1)) {
+        if( c[it+2] == 0 ) it += 2;
+        else it += 1;
+        jumps++;
+    }
+    return jumps;
 }
 
 int main() {
@@ -22,7 +29,7 @@ int main() {
 
     vector<int> c(n_clouds);
 
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n_clouds; i++) {
         int c_item = stoi(c_temp[i]);
         c[i] = c_item;
     }
@@ -33,12 +40,14 @@ int main() {
     return 0;
 }
 
-vector<string> split_string8string input_string) {
+vector<string> split_string(string input_string) {
     string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
         return x == y and x == ' ';
     });
 
-    input_string.erase(new_end, input_string.length() - 1] == ' ') {
+    input_string.erase(new_end, input_string.end());
+    
+    while(input_string[input_string.length() - 1] == ' ') {
         input_string.pop_back();
     }
 
