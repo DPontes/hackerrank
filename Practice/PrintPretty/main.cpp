0e515be8d40bb7d95d399d207082469437e6dec7
DPontes <dpontes@protonmail.com>
2019-10-15 12:40:47 +0200

    Added filter for CppLint

--- Practice/PrintPretty/main.cpp
+++ Practice/PrintPretty/main.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
-#include <iomanip> 
+#include <iomanip>
 
-using namespace std;
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::setw;
+using std::ios;
+using std::internal;
+
 
 int main() {
     int T; cin >> T;
     cout << setiosflags(ios::uppercase);
     cout << setw(0xf) << internal;
-    while(T--) {
+    while (T--) {
         double A; cin >> A;
         double B; cin >> B;
         double C; cin >> C;
 
-        /* Enter your code here */
-
+    cout << "\n0x" << A << "\n" << B << "\n" << C << endl;
     }
     return 0;
-
 }
