cc4887d064e2e6b0c323e163dfc7f27b950f6926
DPontes <dpontes@protonmail.com>
2019-10-21 14:54:08 +0200

    Initial commit

---
+++ Practice/RectangleArea/README.md
@@ -0,0 +1,17 @@
+# Print Pretty
+
+[Link to problem description](https://www.hackerrank.com/challenges/prettyprint/problem)
+
+To compile and run the program with the input file:
+
+```
+> g++ main.cpp -o run.out -std=c++14
+> ./run.out < input.txt
+```
+
+## Expected output
+
+```
+10 5
+50
+```
---
+++ Practice/RectangleArea/main.cpp
@@ -0,0 +1,31 @@
+nclude <iostream>
+
+using namespace std;
+/*
+ * Create classes Rectangle and RectangleArea
+ */
+
+
+int main() {
+    /*
+     * Declare a RectangleArea object
+     */
+    RectangleArea r_area;
+
+    /*
+     * Read the width and height
+     */
+    r_area.read_input();
+
+    /*
+     * Print the width and height
+     */
+    r_area.Rectangle::display();
+
+    /*
+     * Print the area
+     */
+    r_area.display();
+
+    return 0;
+}
