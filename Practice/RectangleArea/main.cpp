d054aa2c95fa6e7c98e476215589bb7b170c2de1
DPontes <dpontes@protonmail.com>
2019-10-23 16:05:24 +0200

    Works and passes all TCs

--- Practice/RectangleArea/main.cpp
+++ Practice/RectangleArea/main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 
 using namespace std;
-/*
- * Create classes Rectangle and RectangleArea
- */
 
+class Rectangle {
+    public:
+        int width;
+        int height;
+        void display() {
+            cout << width << " " << height << endl;
+        }
+};
+
+class RectangleArea : public Rectangle {
+    public:
+        void read_input() {
+            cin >> width;
+            cin >> height;
+        }
+        void display() {
+            cout << width * height;
+        }
+};
 
 int main() {
     /*
