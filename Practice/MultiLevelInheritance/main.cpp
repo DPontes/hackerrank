4bf1427ce942c99f610ee38663825869637283b6
DPontes <dpontes@protonmail.com>
2019-10-23 16:23:25 +0200

    Initial commit

--- Practice/MultiLevelInheritance/main.cpp
+++ Practice/MultiLevelInheritance/main.cpp
@@ -5,3 +5,27 @@
 #include <algorithm>
 
 using namespace std;
+
+class Triangle {
+ public:
+    void triangle() {
+        cout << "I am a triangle" << endl;
+    }
+};
+
+class Isosceles : public Triangle {
+ public:
+    void isosceles() {
+        cout << "I am an isosceles triangle" << endl;
+    }
+};
+
+// Write your code here
+
+int main() {
+    Equilateral eqr;
+    eqr.equilateral();
+    eqr.isosceles();
+    eqr.triangle();
+    return 0;
+}
