4b39a561dda4d275343dedbdc1029c91b4cb8a86
DPontes <dpontes@protonmail.com>
2019-10-23 16:18:56 +0200

    Fixes to comply with CPPlint

---
+++ Practice/MultiLevelInheritance/README.md
@@ -0,0 +1,18 @@
+# Multi Level Inheritance
+
+[Link to problem description](https://www.hackerrank.com/challenges/multi-level-inheritance-cpp/problem)
+
+To compile and run the program with the input file:
+
+```
+> g++ main.cpp -o run.out -std=c++14
+> ./run.out
+```
+
+## Expected output
+
+```
+I am an equilateral triangle
+I am an isosceles triangle
+I am a triangle
+```
--- Practice/RectangleArea/main.cpp
+++ Practice/RectangleArea/main.cpp
@@ -3,23 +3,23 @@
 using namespace std;
 
 class Rectangle {
-    public:
-        virtual void display() const {
-            cout << width << " " << height << endl;
-        }
-    protected:
-        int width;
-        int height;
+ public:
+    virtual void display() const {
+        cout << width << " " << height << endl;
+    }
+ protected:
+    int width;
+    int height;
 };
 
 class RectangleArea : public Rectangle {
-    public:
-        void read_input() {
-            cin >> width >> height;
-        }
-        void display() const override {
-            cout << width * height << endl;
-        }
+ public:
+    void read_input() {
+        cin >> width >> height;
+    }
+    void display() const override {
+        cout << width * height << endl;
+    }
 };
 
 int main() {
