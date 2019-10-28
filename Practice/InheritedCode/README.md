d65cfdedc923aa1ec4fa9487ea99879bc96aea29
DPontes <dpontes@protonmail.com>
2019-10-28 14:33:16 +0100

    Works, passes all TCs

---
+++ Practice/InheritedCode/README.md
@@ -0,0 +1,18 @@
+# Inherited Code
+
+[Link to problem description](https://www.hackerrank.com/challenges/inherited-code/problem)
+
+To compile and run the program with the input file:
+
+```
+> c++ main.cpp -o run.out -std=c++14
+> ./run.out < input.txt
+```
+
+## Expected output
+
+```
+Valid
+Too short: 2
+Invalid
+```
---
+++ Practice/InheritedCode/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <exception>
+using namespace std;
+
+// Define your exception
+class BadLengthException {
+ private:
+    int n;
+ public:
+    BadLengthException(int errornumber) { n = errornumber; }
+    int what() { return n; }
+};
+
+bool checkUsername(string username) {
+    bool isValid = true;
+    int n = username.length();
+    if(n < 5) throw BadLengthException(n);
+    for (int i = 0; i < n-1; ++i) {
+        if(username[i] == 'w' && username[i+1] == 'w') isValid = false;
+    }
+    return isValid;
+}
+
+int main() {
+    int T; cin >> T;
+    while(T--) {
+        string username;
+        cin >> username;
+        try {
+            bool isValid = checkUsername(username);
+            if(isValid) cout << "Valid" << endl;
+            else cout << "Invalid" << endl;
+        } catch (BadLengthException e) {
+            cout << "Too short: " << e.what() << endl;
+        }
+    }
+    return 0;
+}
