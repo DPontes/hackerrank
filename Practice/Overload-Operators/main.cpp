8de006ebac597b80825b6e4af05582282a67e2cf
DPontes <dpontes@protonmail.com>
2019-10-28 10:08:24 +0100

    Works, passes all TCs

--- Practice/Overload-Operators/main.cpp
+++ Practice/Overload-Operators/main.cpp
@@ -25,8 +25,14 @@ class Complex {
 };
 
 // Overload operators + and << for the class complex
-// + should add two complex numbers as (a+ib) + (c+id) = (a+c) + i(b+d)
-// << should print a complex number in the format "a+ib"
+ostream& operator<< (ostream& os, const Complex& c) {
+    return os << c.a << (c.b > 0 ? '+' : '-') << 'i' << c.b;
+}
+
+Complex operator+ (const Complex& a, const Complex& b) {
+    return { a.a + b.a, a.b + b.b };
+}
+
 
 int main() {
     Complex x, y;
@@ -35,9 +41,7 @@ int main() {
     cin >> s2;
     x.input(s1);
     y.input(s2);
-    Complex z = x + y;
-    cout << z zz endl;
     y.input(s2);
     Complex z = x + y;
-    cout << z zz endl;
+    cout << z << endl;
 }
