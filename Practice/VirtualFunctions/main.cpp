0cf217fed4bb260a596ef4b16888bc01c1d55182
DPontes <dpontes@protonmail.com>
2019-10-29 15:33:06 +0100

    Works, passes all TCs

--- Practice/VirtualFunctions/main.cpp
+++ Practice/VirtualFunctions/main.cpp
@@ -5,6 +5,58 @@
 #include <algorithm>
 using namespace std;
 
+class Person {
+ public:
+    int64_t age;
+    string name;
+    virtual void getdata() { cin >> this->name >> this-> age; }
+    virtual void putdata() { cout << this->name << " " << this->age << endl; }
+};
+
+
+class Professor : public Person {
+ public:
+    Professor() { this->cur_id = ++id; }
+    int64_t cur_id;
+    int64_t publications;
+    static int64_t id;
+    void getdata() { cin >> this->name >> this->age >> this->publications; }
+    void putdata() { cout << this->name << " "
+                          << this->age << " "
+                          << this->publications << " "
+                          << cur_id << endl;
+    }
+};
+int64_t Professor::id = 0;
+
+
+class Student : public Person {
+    const int64_t NumOfMarks = 6;
+    int64_t marksSum = 0;
+ public:
+    Student() { this->cur_id = ++id; }
+    static int64_t id;
+    int64_t cur_id;
+    vector<int64_t> marks;
+    void getdata() {
+        cin >> this-> name >> this->age;
+        int64_t mark;
+        for (int i = 0; i < NumOfMarks; ++i) {
+            cin >> mark;
+            marks.push_back(mark);
+        }
+    }
+    void putdata() {
+        for (auto& n : marks) marksSum += n;
+        cout << this->name << " "
+             << this->age << " "
+             << marksSum << " "
+             << this->cur_id << endl;
+    }
+};
+int64_t Student::id = 0;
+
+
 int main() {
     int n, val;
     cin >> n;
