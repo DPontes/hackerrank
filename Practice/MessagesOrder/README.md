12a860b8fad0ac33ce368ae1b93a1e00dc57caaf
DPontes <dpontes@protonmail.com>
2019-10-29 17:20:03 +0100

    Initial commit, main.cpp is CppLint non-compliant due to a runtime/references error

---
+++ Practice/MessagesOrder/README.md
@@ -0,0 +1,19 @@
+# Messages order
+
+[Link to problem description](https://www.hackerrank.com/challenges/messages-order/problem)
+
+To compile and run the program with the input file:
+
+```
+> c++ main.cpp -o run.out -std=c++14
+> ./run.out < input.txt
+```
+
+## Expected output for input.txt
+
+```
+Alex
+Hello Monique!
+What'up?
+Not much :(
+```
---
+++ Practice/MessagesOrder/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Message {
+ public:
+    Message() {}
+    const string& get_text() {
+    }
+};
+
+class MessageFactory {
+ public:
+    MessageFactory() {}
+    Message create_message(const string& text) {
+    }
+};
+
+class Recipient {
+ public:
+    Recipient() {}
+    void recieve(const Message& msg) { messages_.push_back(msg); }
+    void print_messages() {
+        fix_order();
+        for (auto& msg : messages_) {
+            cout << msg.get_text() << endl;
+        }
+        messages_clear();
+    }
+ private:
+    void fix_order() { sort(messages_.begin(), messages_.end()); }
+    vector<Message> messages_;
+};
+
+class Network {
+ public:
+    static void send_messages(vector<Message> messages, Recipient& recipient) {
+    // simulates the unpredictable network, where sent messages
+    // might arrive in unscpecified order
+        random_shuffle(messages.begin(), messages.end());
+        for (auto& msg : messages) recipient.recieve(msg);
+    }
+};
+
+int main() {
+    MessageFactory message_factory;
+    Recipient recipient;
+    vector<Message> messages;
+    string text;
+    while (getline(cin, text)) {
+        messages.push_back(message_factory.create_message(text));
+    }
+    Network::send_messages(messages, recipient);
+    recipient.print_messages();
+}
